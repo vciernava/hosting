02523158ce7b279c2c1c24f6c9cfd95c83b42f6e
Viktor Čierňava <kontakt@viktorciernava.eu>
2024-01-22 12:33:37 +0100

    Opened API to public

--- api/src/index.ts
+++ api/src/index.ts
@@ -12,6 +12,6 @@ app.use(express.json());
 
 setupRoutes(app);
 
-app.listen(3333, () => {
+app.listen(3333, '0.0.0.0', () => {
   console.log('[API] server listening on http://localhost:3333');
 });
